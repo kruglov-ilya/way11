0661a8640d6e365ddf7970c3029b8b61f1dd5b4a
kruglov-ilya <kruglov-il1@yandex.ru>
2023-07-25 03:21:31 +0400

    complete

--- src/js/script.js
+++ src/js/script.js
@@ -18,15 +18,17 @@ window.addEventListener("DOMContentLoaded", () => {
     burger.classList.toggle("active");
   });
 
-  onProjectsSectionHeadler();
+  onProjectsSectionHeadler(getProjectSwitcher());
 });
 
-function onProjectsSectionHeadler() {
+function onProjectsSectionHeadler(onSwitchProjectCallback) {
   const projectsSection = document.getElementById("projects");
   const topSectionPositionOfset = 0;
-  const pause = 500;
+  const pause = 300;
 
-  let projectNumber = 1;
+  const minProjectNumber = 1;
+  const maxProjectNumber = 3;
+  let projectNumber = minProjectNumber;
   let delayStart = Date.now() - pause;
 
   document.addEventListener("scroll", (ev) => {
@@ -38,7 +40,7 @@ function onProjectsSectionHeadler() {
       (scrollToTopOfSection(distanceFromTop) && !isStartState())
     ) {
       scrollIsActive = false;
-    } 
+    }
 
     if (!scrollIsActive) {
       scrollToStartOfSection();
@@ -66,11 +68,11 @@ function onProjectsSectionHeadler() {
   }
 
   function isStartState() {
-    return projectNumber == 0;
+    return projectNumber == minProjectNumber - 1;
   }
 
   function isFinishState() {
-    return projectNumber >= 4;
+    return projectNumber >= maxProjectNumber + 1;
   }
 
   function pauseIsEnded() {
@@ -86,15 +88,72 @@ function onProjectsSectionHeadler() {
     projectNumber++;
     console.log("To up!");
     console.log("Project number:", projectNumber);
+    if (projectNumber <= maxProjectNumber)
+      onSwitchProjectCallback(projectNumber);
   }
 
   function toPrevProject() {
     projectNumber--;
     console.log("To down!");
     console.log("Project number:", projectNumber);
+    if (projectNumber >= minProjectNumber)
+      onSwitchProjectCallback(projectNumber);
   }
 
   function scrollToStartOfSection() {
     window.scrollTo(0, projectsSection.offsetTop);
   }
+}
+
+function getProjectSwitcher() {
+  const projectsOffset = 1380;
+  const projectsWrap = document.querySelector(".projects__test");
+  const projectsWrapper = document.querySelectorAll(".projects__wrapper");
+  const bike = document.querySelector(".projects__road-bike");
+  const projectsText0 = document.getElementById("tr0");
+  const projectsText1 = document.getElementById("tr1");
+  const projectsText2 = document.getElementById("tr2");
+
+  let bikePosition = 0;
+
+  function setActiveProject(number) {
+    console.log("Switch!");
+
+    bike.classList.add("tr");
+    setTimeout(() => bike.classList.remove("tr"), 600);
+
+    if (number == 1) {
+      projectsWrap.style.transform = "translateX(" + 0 + "px)";
+      projectsWrapper.forEach((item) => item.classList.remove("active"));
+      projectsWrapper[0].classList.add("active");
+      projectsText1.classList.remove("active");
+      projectsText2.classList.remove("active");
+      projectsText0.classList.add("active");
+      bikePosition = 0;
+    }
+    if (number == 2) {
+      projectsWrap.style.transform = "translateX(" + -projectsOffset + "px)";
+      projectsWrapper.forEach((item) => item.classList.remove("active"));
+      projectsWrapper[1].classList.add("active");
+      projectsText0.classList.remove("active");
+      projectsText2.classList.remove("active");
+      projectsText1.classList.add("active");
+      bikePosition = 420;
+    }
+    if (number == 3) {
+      projectsWrap.style.transform =
+        "translateX(" + -projectsOffset * 2 + "px)";
+      projectsWrapper.forEach((item) => item.classList.remove("active"));
+      projectsWrapper[2].classList.add("active");
+      projectsText0.classList.remove("active");
+      projectsText1.classList.remove("active");
+      projectsText2.classList.add("active");
+      bikePosition = 840;
+    }
+
+    bike.style.transform = "translateX(" + bikePosition + "px)";
+    bike.style.transition = "all 1s ease-in";
+  }
+
+  return setActiveProject;
 }
